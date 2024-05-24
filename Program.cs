5d60341c318620d064a805dae0a2d47f6af7ac0a
Fedor <fedor122448@gmail.com>
2024-05-24 13:44:09 +0400

    Solve 1-1-b

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace yandex_training;
+﻿// ReSharper disable UnusedMember.Local
+
+namespace yandex_training;
 
 using System;
 
@@ -23,6 +25,15 @@ class ItemReader {
     public string? ReadString() => ReadItem();
 
     public int? ReadInt() => int.TryParse(ReadItem(), out var result) ? result : null;
+
+    public int[] ReadIntArray(int n) {
+        var array = new int[n];
+        for (var i = 0; i < n; i++) {
+            array[i] = ReadInt() ?? 0;
+        }
+
+        return array;
+    }
 }
 
 internal static class Program {
@@ -39,7 +50,28 @@ internal static class Program {
         }
     }
 
+    private static void Train1Class1TaskB() {
+        var sides = Reader.ReadIntArray(3);
+        var maxIndex = 0;
+        var maxSide = -1;
+        for (var i = 0; i < 3; i++) {
+            if (sides[i] > maxSide) {
+                maxIndex = i;
+                maxSide = sides[i];
+            }
+        }
+
+        var subSidesSum = 0;
+        for (var i = 0; i < 3; i++) {
+            if (i != maxIndex) {
+                subSidesSum += sides[i];
+            }
+        }
+
+        Console.WriteLine(maxSide < subSidesSum ? "YES" : "NO");
+    }
+
     public static void Main() {
-        Train1Class1TaskA();
+        Train1Class1TaskB();
     }
 }
